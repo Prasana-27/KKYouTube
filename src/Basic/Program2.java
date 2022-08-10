d966c64d2a36f6c9c1843156a86394e08965f02b
prasana27091996 <prazana18@gmail.com>
2022-08-10 07:42:39 +0530

    Program2 Added

---
+++ src/Basic/Program2.java
@@ -0,0 +1,17 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Please Enter your Name : " );
+		String userName = scan.nextLine();
+		
+		System.out.println("Welcome : " + userName );
+
+	}
+
+}
